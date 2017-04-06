6e92b78af3d252e3e7ccaa3f2c5063f759d52eb2
NatSimon <nathalieosimon@gmail.com>
2017-04-06 12:36:23 -0400

    Updated from Brython Server: 4/6/2017 12:36:23 PM

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -52,7 +52,7 @@ for n in range(1,a):
         entry += "fizz"
     if n%5 == 0:
         entry += "buzz"
-    if i%3 != 0 and i%5 != 0:
+    if n%3 != 0 and n%5 != 0:
         entry = i
 
     fizzbuzz.append(entry)
